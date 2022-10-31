6e16c23f0fac9bf12393ecda75f66379f8e64718
Carson Hartley <chartley1988@gmail.com>
2022-10-30 18:43:53 -0600

    Game logis is done, but not the AI yet

--- script.js
+++ script.js
@@ -1,8 +1,116 @@
+// Game Controller Object
+const gameController = (() => {
+    // Properties
+    let playerTurn = 0; // Which players turn is it?
+    let turnCount = 0; // How many turns have been played?
+    let win = false;
+    // Functions
+    const initializeGame = () => {
+        gameBoard.resetBoard();
+        playerTurn = player1;
+        console.log('Let the game begin!')
+        console.log(`${player1.name}'s turn!`)
+    }
+    //------------------------
+    const playTurn = () => {
+        let board = gameBoard.getCellArray();
+        for(let cell = 0; cell < board.length; cell++) {
+            ['click', 'touchend'].forEach(evt =>
+            board[cell].addEventListener(evt, () => {
+                if(!win){
+                    if(gameBoard.isCellOccupied(cell) === false) {
+                        gameBoard.placeSymbol(playerTurn.boardSymbol, cell);
+                        if(checkForWin()){
+                            endGame();
+                        };
+                        endTurn();
+                        return
+                    } else {
+                        console.log("This cell is already played, choose another!");
+                        return
+                    }
+                }
+            }))
+        }
+    }
+    //------------------------
+    const endTurn = () => {
+        if(!win){
+            if(playerTurn === player1){
+                playerTurn = player2;
+            } else {
+                playerTurn = player1;
+            }
+            turnCount++; 
+            console.log(`End of Turn ${turnCount}`);
+            console.log(`${playerTurn.name}'s turn.`)
+            return
+        }
+    }
+    //------------------------
+    const playGame = () => {
+        initializeGame();
+        playTurn();
+    }
+    //------------------------
+    const endGame = () => {
+        console.log("Game Over!")
+        // setTimeout(initializeGame, 3000);
+    }
+    //------------------------
+    const checkForWin = () => {
+        const board = gameBoard.gameBoardArray;
+        const winConditions = [
+            [board[0],board[1],board[2]],
+            [board[3],board[4],board[5]],
+            [board[6],board[7],board[8]],
+            [board[0],board[3],board[6]],
+            [board[2],board[5],board[8]],
+            [board[0],board[4],board[8]],
+            [board[2],board[4],board[6]]
+        ];
+        winConditions.forEach(condition => {
+            if(checkWinCondition(condition)){
+                console.log(`${playerTurn.name} wins!`);
+                win = true;
+            };
+        });
+        if(win) return(true);
+    }
+    //------------------------
+    const checkWinCondition= (condition) => {
+        const sign = playerTurn.boardSymbol;
+        let winCount = 0;
+        for(let i = 0; i < 3; i++){
+            if(condition[i] === sign){winCount++};
+        }
+        if(winCount === 3){
+            return(true);
+        }
+    };
+    //------------------------
+    return {
+        initializeGame,
+        playGame,
+        endGame,
+    };
+})();
+
 // GameBoard Object
 const gameBoard = (() => {
-    const gameBoardArray = ["X","O","","","","","","",""];
+// Properties 
+    const gameBoardArray = ["","","","","","","","",""];
     const gameBoardCells = document.getElementById("game-board").children;
     const cellArray = Array.from(gameBoardCells);
+    
+// Functions
+    const resetBoard = () => {
+        for (let cell = 0; cell < gameBoardArray.length; cell++) {
+            gameBoardArray[cell] = "";
+        }
+        updateBoard();
+    }
+
     const isCellOccupied = (cell) => { // returns true if theres a marker there, false if empty
         if(gameBoardArray[cell] === "") {
             return(false);
@@ -12,6 +120,7 @@ const gameBoard = (() => {
     }
     const placeSymbol = (playerSymbol, boardCell) => {
         gameBoardArray[boardCell] = playerSymbol;
+        updateBoard();
     }
     const updateBoard = () => {
         for (let cell = 0; cell < gameBoardCells.length; cell++) {
@@ -22,37 +131,14 @@ const gameBoard = (() => {
     const getCellArray = () => {
         return(cellArray);
     }
-    return {updateBoard, placeSymbol, getCellArray, isCellOccupied};
-})();
-
-
-// Game Controller Object
-const gameController = (() => {
-    // determine who the players are, and what their symbols are
-    // determine whether the second player is ai or player 2
-    // determine which player goes first
-    // make move
-    const playTurn = (player) => {
-        let board = gameBoard.getCellArray();
-        for(let cell = 0; cell < board.length; cell++) {
-            ['click', 'touchend'].forEach(evt =>
-            board[cell].addEventListener(evt, () => {
-                if(gameBoard.isCellOccupied(cell) === false) {
-                    gameBoard.placeSymbol(player.boardSymbol, cell);
-                    gameBoard.updateBoard();
-                    endTurn();
-                } else {
-                    console.log("This cell is already played, choose another!");
-                    return
-                }
-
-            }))
-        }
-    }
-    const endTurn = () => {
-        console.log("end Turn");
-    }
-    return {playTurn};
+    return {
+        gameBoardArray,
+        resetBoard,
+        updateBoard,
+        placeSymbol,
+        getCellArray,
+        isCellOccupied
+        };
 })();
 
 
@@ -65,9 +151,8 @@ const Player = (name, boardSymbol) => {
     return {name, boardSymbol, set_ai};
 }
 
-const player1 = Player("Carson", "M");
-const player2 = Player("AI", "T");
-gameBoard.updateBoard();
-gameController.playTurn(player1);
+const player1 = Player("Carson", "C");
+const player2 = Player("Leah", "L");
+gameController.playGame();
 
 
